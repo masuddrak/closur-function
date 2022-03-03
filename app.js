52cacf7bcaaa425d7d7ed6bc52d264c44a32c863
masudrana <masudranaacb@gmail.com>
2022-03-03 08:12:09 +0600

    functional object

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 // simple a closur function
+// const bank=woner=>{
+//     balance=0;
+//     return amount=>{
+//         balance +=amount;
+//         return balance;
+//     }
+// }
+// const mofizBank=bank('mofiz')
+// console.log(mofizBank(100))
+// console.log(mofizBank(100))
+
+
+// functional object
 const bank=woner=>{
     balance=0;
-    return amount=>{
-        balance +=amount;
-        return balance;
+    return {
+        diposit:amount=>{
+            balance +=amount;
+            return balance;
+        },
+        widraw:amount=>{
+            balance -=amount;
+            return balance;
+        },
     }
 }
 const mofizBank=bank('mofiz')
-console.log(mofizBank(100))
-console.log(mofizBank(100))
+console.log(mofizBank.diposit(100))
+console.log(mofizBank.diposit(100))
+console.log(mofizBank.diposit(100))
+console.log(mofizBank.diposit(100))
+console.log(mofizBank.widraw(100))
